d64170b8899e2e89ec158edd60e71c7c8b7262da
smorrow1 <34723728+smorrow1@users.noreply.github.com>
2020-04-21 14:20:04 +0200

    Add files via upload

---
+++ Baumkataster_Visualisierung/Baum.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datashader
+
+
+
+df = pd.read_csv("/Users/morrow/Documents/Baumkataster_Visualisierung/Baum.csv", sep=";")
+
+
+print(df.head())
